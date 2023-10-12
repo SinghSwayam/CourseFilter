81c7113ab2cc46e2813122c4843f743f780b39d1
SinghSwayam <swayamsingh.dev@gmail.com>
2023-10-12 16:11:48 +0530

    CourseFilter

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -3,7 +3,7 @@ import "./Spinner.css"
 
 const Spinner = () => {
   return (
-    <div className=' h-[100vh] flex flex-col justify-center items-center space-y-2'>
+    <div className='  flex flex-col justify-center items-center space-y-2'>
         <div className=' custom-loader'></div>
         <p className=' text-3xl font-semibold'>Loading...</p>
     </div>
